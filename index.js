2f137fa1a300a3b754a1fc49601e51fd494de73b
RoySamson-stack <rsam9009@gmail.com>
2021-09-11 13:22:28 +0300

    maing scritps for each portfolio image

--- index.js
+++ index.js
@@ -15,5 +15,45 @@ $(document).ready(function(){
     $("#portfolio-img").css("background-color", "white");
     $("#work1").toggle();
     $("#hidden1").toggle();
+  });
+  $("#firebase-img").hover(function(){
+    $("#firebase-img").css("background-color", "white");
+    $("#work2").toggle();
+    $("#hidden2").toggle();
+  });
+  $("#security-img").hover(function(){
+    $("#security-img").css("background-color", "white");
+    $("#work3").toggle();
+    $("#hidden3").toggle();
+  });
+  $("#app-img").hover(function(){
+    $("#app-img").css("background-color", "white");
+    $("#work4").toggle();
+    $("#hidden4").toggle();
+  });
+  $("#admin-img").hover(function(){
+    $("#admin-img").css("background-color", "white");
+    $("#work5").toggle();
+    $("#hidden5").toggle();
+  });
+  $("#blog-img").hover(function(){
+    $("#blog-img").css("background-color", "white");
+    $("#work6").toggle();
+    $("#hidden6").toggle();
+  });
+  $("#bar-img").hover(function(){
+    $("#bar-img").css("background-color", "white");
+    $("#work7").toggle();
+    $("#hidden7").toggle();
+  });
+  $("#resturant-img").hover(function(){
+    $("#resturant-img").css("background-color", "white");
+    $("#work8").toggle();
+    $("#hidden8").toggle();
+  });
+  $("#portfolio-img").hover(function(){
+    $("#portfolio-img").css("background-color", "white");
+    $("#work1").toggle();
+    $("#hidden1").toggle();
   })
 })
