ca4ef1e7c3307b8c034982ea7010731b1298f7d8
RoySamson-stack <rsam9009@gmail.com>
2021-09-13 17:35:39 +0300

    fixing the mail chimp api

--- index.js
+++ index.js
@@ -11,40 +11,76 @@ $(document).ready(function(){
     $(".show3").toggle();
     $(".hide3").toggle();
   });
-  $("#portfolio-img").hover(function(){
+  $("#portfolio-img").mouseenter(function(){
     $("#work1").css("opacity", "0.5");
     $("#hidden1").show();
+  }).mouseleave(function(){
+    $("#work1").css("opacity", "0.8");
+    $("#hidden1").hide();
   });
-  $("#firebase-img").hover(function(){
-    $("#work2").css("opacity", "0.5",);
+  $("#firebase-img").mouseenter(function(){
+    $("#work2").css("opacity", "0.5");
     $("#hidden2").toggle();
+  }).mouseleave(function(){
+    $("#work2").css("opacity", "0.8");
+    $("#hidden2").hide();
   });
-  $("#security-img").hover(function(){
+  $("#security-img").mouseenter(function(){
     $("#work3").css("opacity", "0.5");
     $("#hidden3").toggle();
+  }).mouseleave(function(){
+    $("#work3").css("opacity", "1");
+    $("#hidden3").hide();
   });
-  $("#app-img").hover(function(){
+  $("#app-img").mouseenter(function(){
     $("#work4").css("opacity", "0.5");
     $("#hidden4").toggle();
+  }).mouseleave(function(){
+    $("#work4").css("opacity", "0.8");
+    $("#hidden4").hide();
   });
-  $("#admin-img").hover(function(){
+  $("#admin-img").mouseenter(function(){
     $("#work5").css("opacity", "0.5");
     $("#hidden5").toggle();
+  }).mouseleave(function(){
+    $("#work5").css("opacity", "0.8");
+    $("#hidden5").hide();
   });
-  $("#blog-img").hover(function(){
+  $("#blog-img").mouseenter(function(){
     $("#work6").css("opacity", "0.5");
     $("#hidden6").toggle();
+  }).mouseleave(function(){
+    $("#work6").css("opacity", "0.8");
+    $("#hidden6").hide();
   });
-  $("#bar-img").hover(function(){
+  $("#bar-img").mouseenter(function(){
     $("#work7").css("opacity", "0.5");
     $("#hidden7").toggle();
-  });
-  $("#resturant-img").hover(function(){
+  }).mouseleave(function(){
+    $("#work7").css("opacity", "0.8");
+    $("#hidden7").hide();
+  });;
+  $("#resturant-img").mouseenter(function(){
     $("#work8").css("opacity", "0.5");
     $("#hidden8").toggle();
+  }).mouseleave(function(){
+    $("#work8").css("opacity", "0.8");
+    $("#hidden8").hide();
+  }); 
+  var target = document.getElementById('mce-success-response');
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (target.innerHTML === "Thank you for contact us") {
+      target.innerHTML = "Check your email!";
+    }
   });
-  $("#portfolio-img").hover(function(){
-    $("#work1").css("opacity", "0.5");
-    $("#hidden1").toggle();
-  })
+});
+
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true };
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
 })
